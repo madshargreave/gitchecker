87220ab4362fdcf863d68e5df1899fc94516ed2b
Mads Hargreave <mads.hargreave@gmail.com>
2016-02-15 01:32:39 +0100

    ada

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :githubber, Githubber.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "http", host: "gitchecker.com", port: 80],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
